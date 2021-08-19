e7f04b60c9de558ba6bec62bee9e6827b3193c9a
Thaarik <a.thaarikahamed@gmail.com>
2021-08-19 11:03:11 +0530

    Additional mobile responsive updates

--- src/Components/MainHome/MainHome.js
+++ src/Components/MainHome/MainHome.js
@@ -5,24 +5,24 @@ const MainHome=()=>{
     return(
         <div className="grid">
             <div className="image1"><img className="grid1" src={image1} alt="main-img"/>
-                {/* <div className="titlebox"> */}
+                
                     <p className="Title1">Title of vertical gallery</p>
                     <p className="Subtitle">Travel/August 21 2017</p>
-                {/* </div> */}
+
             </div>
             <div className="image2"><img className="grid2" src={image2} alt="main-img"/>
-                {/* <div className="titlebox"> */}
+                
                     <p className="Title2" id="one">The Sound cloud</p>
                     <p className="Title2" id="two">You loved is doomed</p>
                     <p className="Subtitle">Travel/August 21 2017</p>
-                {/* </div> */}
+                
             </div>
             <div className="image3"><img className="grid3" src={image2} alt="main-img"/>
-                {/* <div className="titlebox"> */}
+                
                     <p className="Title3" id="three">The Sound cloud</p>
                     <p className="Title3" id="four">You loved is doomed</p>
                     <p className="Subtitle">Travel/August 21 2017</p>
-                {/* </div> */}
+                
             </div>
                 
         </div>
